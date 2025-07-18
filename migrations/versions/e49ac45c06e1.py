b051c399e5124f1091e30532cd9c8df956d850c3
otazzu <srotazu@gmail.com>
2025-07-18 17:29:49 +0000

    Ready to deploy v1.1

--- migrations/versions/e49ac45c06e1.py
+++ migrations/versions/e49ac45c06e1.py
@@ -1,6 +1,6 @@
 """empty message
 
-Revision ID: c72929bfb1f1
+Revision ID: e49ac45c06e1
 Revises: 
 Create Date: 2025-07-16 19:07:50.916960
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c72929bfb1f1'
+revision = 'e49ac45c06e1'
 down_revision = None
 branch_labels = None
 depends_on = None
